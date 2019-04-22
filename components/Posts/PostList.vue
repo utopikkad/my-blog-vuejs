3d030ac3e5bb33b55b01f63b275e5e2d8a63903e
utopikkad <45292849+utopikkad@users.noreply.github.com>
2019-04-22 17:39:52 +0200

    first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,56 @@
+<template>
+     <section class="post-list">
+        <PostPreview 
+        id="1"
+        :is-admin="isAdmin"
+        thumbnail="https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg"
+        title="Which framework do you need ?"
+        previewText="Angular 2+, React or Vue. Which to use in which case ?"/>
+        <PostPreview 
+        id="2"
+        :is-admin="isAdmin"
+        thumbnail="https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg"
+        title="Consoles are dead ?"
+        previewText="Stadia, cloud gaming. Is it the requiem of consoles ?"/>
+        <PostPreview 
+        id="3"
+        :is-admin="isAdmin"
+        thumbnail="https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg"
+        title="GAFAM or BATX ?"
+        previewText="technological warfare, where is Europe ?"/>
+        <PostPreview 
+        id="4"
+        :is-admin="isAdmin"
+        thumbnail="https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg"
+        title="Smartphone or smartpeople ?"
+        previewText="Do I really need my brain to think ?"/>
+    </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+    components: {
+        PostPreview
+    },
+    props : {
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
---
+++ pages/admin/_postid/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost"/>
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Kaddour',
+                title: 'My awesome post',
+                content: 'Super amazing, thanks for that !',
+                thumbnailLink: 'https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg'
+            }
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+
+</style>
+
